99e378a09741162b7bed701fa341edf230fd36d5
vincent <vincent.riesop@gmail.com>
2018-10-04 16:39:51 +0200

    neotree and keybindings

--- init.el
+++ init.el
@@ -16,36 +16,45 @@
 (require 'use-package)
 (add-to-list 'load-path "~/.emacs.d/vince")
 
-
 ;; == evil
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
 
-;; == Linum
-(require 'linum-relative)
-
 ;; == Helm
 (require 'setup-helm)
 (require 'setup-helm-gtags)
 
+;; == NeoTree
+(require 'neotree)
 
-;; == sublimity
-;;(require 'sublimity)
-;;(require 'sublimity-scroll)
-;;(require 'sublimity-map) ;; experimental
-;;(require 'sublimity-attractive)
-;; ==== Keybindings 
+
+;; ==== Keybindings
+
+;; == NeoTree
+(global-set-key [f8] 'neotree-toggle)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(add-hook 'neotree-mode-hook
+          (lambda ()
+            (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+            (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+            (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+            (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+
 ;; == windmove (shift+arrowkey to switch window)
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
+(global-set-key [(super j)]  'windmove-down)
+(global-set-key [(super k)]  'windmove-up)
+(global-set-key [(super h)]  'windmove-left)
+(global-set-key [(super l)]  'windmove-right)
 
 
 ;; ==== Style
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-
+(scroll-bar-mode -1)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -56,7 +65,7 @@
  '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
-    ("e03d2f414fb109f3920752b10b92323697174f49d577da9e69979edbb147a921" default)))
+    ("d3cf4dfbff156bdd42c874c48b35cd86a204934ee6ddb3462ab9d9ea4d5a0775" "e03d2f414fb109f3920752b10b92323697174f49d577da9e69979edbb147a921" default)))
  '(package-selected-packages
    (quote
     (sublimity zenburn-theme helm-gtags helm-projectile use-package helm-ack helm linum-relative evil))))
