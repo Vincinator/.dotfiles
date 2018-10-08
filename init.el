36ea86fcdf62952c34aceb9c314bd82cf8ec6ee1
vincent <vincent.riesop@gmail.com>
2018-10-08 13:50:25 +0200

    window movement ace and magit

--- init.el
+++ init.el
@@ -25,6 +25,22 @@
 (require 'setup-helm)
 (require 'setup-helm-gtags)
 
+(setq helm-exit-idle-delay 0)
+
+;; == projectile
+
+(require 'projectile)
+(define-key projectile-mode-map (kbd "M-p") #'projectile-command-map)
+(setq projectile-indexing-method 'turbo-alien)
+
+(projectile-global-mode)
+
+(setq projectile-completion-system 'helm)
+(setq projectile-enable-caching t)
+(helm-projectile-on)
+
+
+
 
 ;; == CEDET
 
@@ -51,12 +67,22 @@
 (require 'shell-pop)
 
 
+;; == ace-window
+
+(require 'ace-window)
+(global-set-key (kbd "M-o") 'ace-window)
+
 ;; ==== Startup Config
 (setq inhibit-startup-screen t)
 
+(add-hook 'dired-mode-hook
+      (lambda ()
+        (dired-hide-details-mode)))
 
 
 ;; ==== Keybindings
+
+
 ;; == Window resize keys
 (global-set-key (kbd "<C-up>") 'shrink-window)
 (global-set-key (kbd "<C-down>") 'enlarge-window)
@@ -65,17 +91,28 @@
 ;; == open terminal (shell-pop)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ ;; If there is more than
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("d3cf4dfbff156bdd42c874c48b35cd86a204934ee6ddb3462ab9d9ea4d5a0775" "e03d2f414fb109f3920752b10b92323697174f49d577da9e69979edbb147a921" default)))
+ '(package-selected-packages
+   (quote
+    (ace-window helm-fuzzy-find shell-pop projectile-speedbar sr-speedbar function-args ggtags 0blayout sublimity zenburn-theme helm-gtags helm-projectile use-package helm-ack helm linum-relative evil)))
+ '(semantic-mode t)
  '(shell-pop-default-directory "/home/vincent/")
- '(shell-pop-shell-type (quote ("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell)))))
+ '(shell-pop-full-span t)
+ '(shell-pop-shell-type
+   (quote
+    ("ansi-term" "*ansi-term*"
+     (lambda nil
+       (ansi-term shell-pop-term-shell)))))
  '(shell-pop-term-shell "/bin/zsh")
  '(shell-pop-universal-key "C-1")
- '(shell-pop-window-size 30)
- '(shell-pop-full-span t)
- '(shell-pop-window-position "bottom"))
+ '(shell-pop-window-position "bottom")
+ '(shell-pop-window-size 30))
 
 
 ;; == NeoTree
@@ -97,16 +134,32 @@
 (global-set-key [(super l)]  'windmove-right)
 
 
+;; == Magit
+
+
+
+
 ;; == Sr-Speedbar
 (define-key speedbar-mode-map (kbd "TAB") 'speedbar-edit-line)
 
 ;; ==== Style
+(set-face-attribute 'default nil :height 180)
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
 ;; NeoTree
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -121,12 +174,16 @@
     ("d3cf4dfbff156bdd42c874c48b35cd86a204934ee6ddb3462ab9d9ea4d5a0775" "e03d2f414fb109f3920752b10b92323697174f49d577da9e69979edbb147a921" default)))
  '(package-selected-packages
    (quote
-    (shell-pop projectile-speedbar sr-speedbar function-args ggtags 0blayout sublimity zenburn-theme helm-gtags helm-projectile use-package helm-ack helm linum-relative evil)))
- '(semantic-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
+    (magit ace-window helm-fuzzy-find shell-pop projectile-speedbar sr-speedbar function-args ggtags 0blayout sublimity zenburn-theme helm-gtags helm-projectile use-package helm-ack helm linum-relative evil)))
+ '(semantic-mode t)
+ '(shell-pop-default-directory "/home/vincent/")
+ '(shell-pop-full-span t)
+ '(shell-pop-shell-type
+   (quote
+    ("ansi-term" "*ansi-term*"
+     (lambda nil
+       (ansi-term shell-pop-term-shell)))))
+ '(shell-pop-term-shell "/bin/zsh")
+ '(shell-pop-universal-key "C-1")
+ '(shell-pop-window-position "bottom")
+ '(shell-pop-window-size 30))
